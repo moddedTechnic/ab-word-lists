c7445b835600bb99e7d8bf3ff7bff00ccc516010
Jonathan Leeming <jonathan@leeming.dev>
2023-12-29 23:59:41 +0000

    mvp complete

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue'
+
+const audioSource = ref<HTMLAudioElement | null>(null)
+const runningTest = ref(false)
+const currentTest = ref(1)
+
+const testLabel = computed(() => currentTest.value.toString().padStart(2, '0'))
+
+function startTest() {
+  if (runningTest.value || !audioSource.value)
+    return;
+  runningTest.value = true
+  audioSource.value.play()
+}
+
+function stopTest() {
+  if (!runningTest.value || !audioSource.value)
+    return;
+  runningTest.value = false
+  audioSource.value.pause()
+  audioSource.value.currentTime = 0
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="test-controls">
+    <p class="currentTest">
+      Current Test
+      <span>{{ currentTest }}</span>
+    </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div class="controls">
+    <button>Calibrate</button>
+
+    <button v-if="runningTest" @click.prevent="stopTest">Cancel Test</button>
+    <button v-else @click.prevent="startTest">Begin Test</button>
+  </div>
+  <audio ref="audioSource">
+    <source :src="`/audio/list-${testLabel}.wav`" type="audio/wav" />
+  </audio>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.controls {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 2rem;
+
+  font-size: 1.5rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.currentTest {
+  font-size: 1.25rem;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.currentTest span {
+  margin-left: 0.5rem;
+  font-size: 1.75rem;
+  font-weight: 700;
 }
 </style>
