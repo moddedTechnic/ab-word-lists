ded418ee1a63852e5d856569f6b4f71c6c88ec42
Jonathan Leeming <jonathan@leeming.dev>
2023-12-30 13:17:46 +0000

    added calibration and started wordlists

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import { computed, ref } from 'vue'
 
 const words = [
-  ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10'],
-  ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10'],
+  ['ship', 'rug', 'fan', 'cheek', 'haze', 'dice', 'both', 'well', 'jot', 'move'],
+  ['fish', 'duck', 'gap', 'cheese', 'rail', 'hive', 'bone', 'wedge', 'moss', 'tooth'],
   ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'],
   ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10'],
-  ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'e10'],
-  ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10'],
+  ['fib', 'thatch', 'sum', 'heel', 'wide', 'rake', 'goes', 'shop', 'vet', 'june'],
+  ['fill', 'catch', 'thumb', 'heap', 'wise', 'rave', 'goat', 'shone', 'bed', 'juice'],
   ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10'],
   ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10'],
   ['i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9', 'i10'],
@@ -17,7 +17,9 @@ const words = [
 ]
 
 const audioSources = ref<HTMLAudioElement[]>([])
+const calibrationTone = ref<HTMLAudioElement | null>(null)
 const scores = ref<number[]>(Array(12).fill(0))
+const playingCalibration = ref(false)
 const runningTest = ref(false)
 const currentTest = ref(1)
 const currentAudio = computed(() => audioSources.value[currentTest.value - 1])
@@ -50,13 +52,27 @@ function randomTest(): number {
   while ((test = Math.floor(Math.random() * 12) + 1) === currentTest.value);
   return test
 }
+
+function calibrate() {
+  if (playingCalibration.value || !calibrationTone.value)
+    return
+  playingCalibration.value = true
+  calibrationTone.value.play()
+}
+function stopCalibration() {
+  if (!playingCalibration.value || !calibrationTone.value)
+    return
+  playingCalibration.value = false
+  calibrationTone.value.pause()
+  calibrationTone.value.currentTime = 0
+}
 </script>
 
 <template>
   <div class="status-info">
     <p>
       Current Test
-      <span>{{ currentTest }}</span>
+      <span>{{ currentTest >= 9 ? currentTest + 2 : currentTest }}</span>
     </p>
 
     <p>
@@ -74,7 +90,10 @@ function randomTest(): number {
   </div>
 
   <div class="controls">
-    <button>Calibrate</button>
+    <button v-if="playingCalibration" @click.prevent="stopCalibration">
+      End Calibration
+    </button>
+    <button v-else @click.prevent="calibrate">Calibrate</button>
 
     <button v-if="runningTest" @click.prevent="stopTest">Cancel Test</button>
     <button v-else @click.prevent="startTest">Begin Test</button>
@@ -111,6 +130,10 @@ function randomTest(): number {
       </li>
     </template>
   </ul>
+
+  <audio ref="calibrationTone" @ended="stopCalibration">
+    <source src="/audio/calibration.wav" type="audio/wav" />
+  </audio>
 </template>
 
 <style scoped>
@@ -121,6 +144,7 @@ function randomTest(): number {
 .test-controls {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   gap: 2rem;
@@ -131,16 +155,19 @@ function randomTest(): number {
 .controls {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   gap: 2rem;
 
+  margin-bottom: 2rem;
   font-size: 1.5rem;
 }
 
 .status-info {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: space-evenly;
 
@@ -156,8 +183,9 @@ function randomTest(): number {
 .word {
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-between;
 
   font-size: 1.25rem;
 }
