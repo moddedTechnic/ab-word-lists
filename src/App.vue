8725dc78d3b04ef516b81eac9e5080f8a40ef6ff
Jonathan Leeming <jonathan@leeming.dev>
2023-12-30 12:15:39 +0000

    added scoring

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 
+const words = [
+  ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10'],
+  ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10'],
+  ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'],
+  ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10'],
+  ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'e10'],
+  ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10'],
+  ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10'],
+  ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10'],
+  ['i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9', 'i10'],
+  ['j1', 'j2', 'j3', 'j4', 'j5', 'j6', 'j7', 'j8', 'j9', 'j10'],
+  ['k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'k8', 'k9', 'k10'],
+  ['l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8', 'l9', 'l10'],
+]
+
 const audioSources = ref<HTMLAudioElement[]>([])
+const scores = ref<number[]>(Array(12).fill(0))
 const runningTest = ref(false)
 const currentTest = ref(1)
 const currentAudio = computed(() => audioSources.value[currentTest.value - 1])
+const currentWordlist = computed(() => words[currentTest.value - 1])
+const score = computed(() => scores.value.reduce((a, b) => a + b))
 
 function startTest() {
-  console.log(currentAudio.value)
   if (runningTest.value || !currentAudio.value)
     return;
   runningTest.value = true
   currentAudio.value.play()
+  scores.value.fill(0)
 }
 
 function stopTest() {
@@ -35,12 +53,19 @@ function randomTest(): number {
 </script>
 
 <template>
-  <div class="test-info">
-    <p class="currentTest">
+  <div class="status-info">
+    <p>
       Current Test
       <span>{{ currentTest }}</span>
     </p>
 
+    <p>
+      Score
+      <span>{{ score }}</span>
+    </p>
+  </div>
+
+  <div class="test-info">
     <div class="test-controls">
       <button @click.prevent="currentTest--">Previous</button>
       <button @click.prevent="currentTest = randomTest()">Random</button>
@@ -60,6 +85,32 @@ function randomTest(): number {
       <source :src="`/audio/list-${i.toString().padStart(2, '0')}.wav`" type="audio/wav" />
     </audio>
   </template>
+
+  <ul>
+    <template v-for="i in currentWordlist.length">
+      <li class="word">
+        <p>{{ currentWordlist[i - 1] }}</p>
+
+        <div>
+          <input type="radio" :value="3" :id="`${currentWordlist[i - 1]}-3`" :name="currentWordlist[i - 1]"
+            v-model="scores[i - 1]" :v-bind:value="3" />
+          <label :for="`${currentWordlist[i - 1]}-3`">3</label>
+
+          <input type="radio" :value="2" :id="`${currentWordlist[i - 1]}-2`" :name="currentWordlist[i - 1]"
+            v-model="scores[i - 1]" :v-bind:value="2" />
+          <label :for="`${currentWordlist[i - 1]}-2`">2</label>
+
+          <input type="radio" :value="1" :id="`${currentWordlist[i - 1]}-1`" :name="currentWordlist[i - 1]"
+            v-model="scores[i - 1]" :v-bind:value="1" />
+          <label :for="`${currentWordlist[i - 1]}-1`">1</label>
+
+          <input type="radio" :value="0" :id="`${currentWordlist[i - 1]}-0`" :name="currentWordlist[i - 1]"
+            v-model="scores[i - 1]" :v-bind:value="0" />
+          <label :for="`${currentWordlist[i - 1]}-0`">0</label>
+        </div>
+      </li>
+    </template>
+  </ul>
 </template>
 
 <style scoped>
@@ -87,13 +138,44 @@ function randomTest(): number {
   font-size: 1.5rem;
 }
 
-.currentTest {
+.status-info {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+
   font-size: 1.25rem;
 }
 
-.currentTest span {
+.status-info span {
   margin-left: 0.5rem;
   font-size: 1.75rem;
+  font-weight: 700;
+}
+
+.word {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+
+  font-size: 1.25rem;
+}
+
+.word p {
+  margin: 0;
+}
+
+.word input {
+  display: none;
+}
+
+.word label {
+  margin-right: 1rem;
+  font-weight: 300;
+}
+
+.word input:checked+label {
   font-weight: 700;
 }
 </style>
